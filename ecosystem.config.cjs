e85d46d81bc0deb3ff666a43353f7e50f3322b40
benPeng <benPeng@gmail.com>
2024-08-07 13:38:11 +0800

    build test

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,13 +1,13 @@
 const path = require('path')
 
 const nextEXE = path.normalize('node_modules/next/dist/bin/next')
+// pm2 start ecosystem.config.cjs --env development --attach
 // pm2 start ecosystem.config.cjs --env production --attach
-// pm2 deploy production setup
 
 module.exports = {
   apps: [
     {
-      name: 'cutYuanProfit',
+      name: 'sFinance',
       script: nextEXE,
       env_development: {
         NODE_ENV: 'development',
@@ -27,9 +27,9 @@ module.exports = {
     production: {
       user: 'dls',
       host: '192.168.10.124',
-      ref: 'origin/main',
-      repo: 'http://220.135.67.240:8880/ben_peng/landingstyple1/-/tree/so.git',
-      path: '/var/www/bee/landingStyle1/',
+      ref: 'origin/sFinance',
+      repo: 'http://220.135.67.240:8880/ben_peng/cutyuanprofit.git',
+      path: '/var/www/bee/sFinance/',
       'pre-setup': 'yarn && yarn build && pm2 reload ecosystem.config.cjs --env production',
       'post-deploy': 'sudo nginx -s reload',
     },
